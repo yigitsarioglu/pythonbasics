1a180bbfb3aa830bce27606ee026b65d2f9c5c13
yigitsarioglu <yigitsarioglu@gmail.com>
2022-09-04 13:29:56 +0300

    python basic codings

---
+++ section5/while.py
@@ -0,0 +1,15 @@
+# 1-100 e kadar olan sayıları yazdıralım
+
+# x=0
+
+# while x < 100 :
+#     print (x)
+#     x = x+1
+
+
+name = ""  # false
+
+while not name.strip()  :
+    name = input ("isminizi giriniz : ")
+   
+print(f'Merhaba {name} ')   
